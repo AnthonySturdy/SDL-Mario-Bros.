c72dbcac95479cfc54d7406d37d1ee8fb76f6afc
Anthony Sturdy <anthonysturdy123@gmail.com>
2019-02-25 00:18:56 +0000

    More Level editor

--- AnthonySturdy_Engine_GEC/AnthonySturdy_Engine_GEC/GameScreen_LevelEditor.cpp
+++ AnthonySturdy_Engine_GEC/AnthonySturdy_Engine_GEC/GameScreen_LevelEditor.cpp
@@ -1,16 +1,12 @@
 #include "GameScreen_LevelEditor.h"
 
 // TODO:	- Add more Sprites
-//			- Add erasing
 //			- Add sprite selection
 //			- Make maps playable
 
 GameScreen_LevelEditor::GameScreen_LevelEditor(SDL_Renderer* renderer, int _mapSizeX, int _mapSizeY) : GameScreen(renderer) {
 	mapSizeX = _mapSizeX;
 	mapSizeY = _mapSizeY;
-	for (int i = 0; i < mapSizeX * mapSizeY; i++) {
-		map.push_back(CLEAR);
-	}
 
 	SetUpLevel();
 }
@@ -26,6 +22,23 @@ bool GameScreen_LevelEditor::SetUpLevel() {
 	texture2 = new Texture2D(mRenderer);
 	texture2->LoadFromFile("Images/Mario.png");
 
+	for (int i = 0; i < 2; i++) {
+		std::stringstream ss;
+		ss << "Images/Spritesheet/" << i << ".png";
+		spritesheet.push_back(new Texture2D(mRenderer));
+		spritesheet[i]->LoadFromFile(ss.str().c_str());
+
+		std::stringstream ss_1;
+		ss_1 << "Images/Spritesheet_Cursor/" << i << ".png";
+		spritesheet_cursor.push_back(new Texture2D(mRenderer));
+		spritesheet_cursor[i]->LoadFromFile(ss_1.str().c_str());
+	}
+
+	//Create map
+	for (int i = 0; i < mapSizeX * mapSizeY; i++) {
+		map.push_back(SPRITE_CLEAR);
+	}
+
 	return false;
 }
 
@@ -33,6 +46,7 @@ void GameScreen_LevelEditor::Render() {
 	int zeroWorldToScreenX, zeroWorldToScreenY;
 	WorldToScreen(0, 0, zeroWorldToScreenX, zeroWorldToScreenY);
 	
+	//Draw background
 	SDL_Rect backgroundRect;
 	backgroundRect.x = zeroWorldToScreenX * TILE_SIZE;
 	backgroundRect.y = zeroWorldToScreenY * TILE_SIZE;
@@ -42,6 +56,7 @@ void GameScreen_LevelEditor::Render() {
 	SDL_RenderFillRect(mRenderer, &backgroundRect);
 	SDL_SetRenderDrawColor(mRenderer, 0, 0, 0, 255);
 
+	//Draw Map
 	for (int x = 0; x < mapSizeX; x++) {
 		for (int y = 0; y < mapSizeY; y++) {
 			RenderMapSprite(map[y * mapSizeX + x],
@@ -49,6 +64,13 @@ void GameScreen_LevelEditor::Render() {
 							(zeroWorldToScreenY + y) * TILE_SIZE);
 		}
 	}
+
+	//Draw Cursor
+	int mouseX, mouseY;
+	SDL_GetMouseState(&mouseX, &mouseY);
+	int wX, wY;
+	ScreenToWorld(mouseX, mouseY, wX, wY);
+	DrawCursor(currentSprite, (zeroWorldToScreenX + wX) * TILE_SIZE, (zeroWorldToScreenY + wY) * TILE_SIZE);
 }
 
 void GameScreen_LevelEditor::Update(float deltaTime, SDL_Event e) {
@@ -57,19 +79,21 @@ void GameScreen_LevelEditor::Update(float deltaTime, SDL_Event e) {
 	
 	int worldMouseX, worldMouseY;
 	ScreenToWorld(mouseX, mouseY, worldMouseX, worldMouseY);
-	EditMap(TEST_SPRITE, worldMouseX, worldMouseY);
 
+	EditMap(currentSprite, worldMouseX, worldMouseY);
 	CameraPanning(mouseX, mouseY);
 }
 
 void GameScreen_LevelEditor::EditMap(unsigned short sprite, int x, int y) {
+	//If mouse is in screen bounds
 	if (x > -1 && y > -1 &&
 		x < mapSizeX && y < mapSizeY) {
+
+		//Drawing tiles
 		if (leftMouseDown) {
 			//Left mouse held
 			map[y * mapSizeX + x] = sprite;
 		}
-
 		if (SDL_GetMouseState(NULL, NULL)&SDL_BUTTON(1)) {
 			//Middle mouse down
 			leftMouseDown = true;
@@ -78,7 +102,25 @@ void GameScreen_LevelEditor::EditMap(unsigned short sprite, int x, int y) {
 			//Middle mouse up
 			leftMouseDown = false;
 		}
+
+		//Erasing tiles
+		if (rightMouseDown) {
+			//Left mouse held
+			map[y * mapSizeX + x] = SPRITE_CLEAR;
+		}
+		if (SDL_GetMouseState(NULL, NULL)&SDL_BUTTON(3)) { 
+			//Middle mouse down
+			rightMouseDown = true;
+			map[y * mapSizeX + x] = SPRITE_CLEAR;
+		} else {
+			//Middle mouse up
+			rightMouseDown = false;
+		}
 	}
+}
+
+void GameScreen_LevelEditor::DrawCursor(unsigned short sprite, int x, int y) {
+	RenderCursorSprite(sprite, x, y);
 }
 
 void GameScreen_LevelEditor::CameraPanning(int mX, int mY) {
@@ -90,7 +132,6 @@ void GameScreen_LevelEditor::CameraPanning(int mX, int mY) {
 		startPanX = mX;
 		startPanY = mY;
 	}
-
 	if (SDL_GetMouseState(NULL, NULL)&SDL_BUTTON(2)) {
 		//Middle mouse down
 		middleMouseDown = true;
@@ -104,14 +145,12 @@ void GameScreen_LevelEditor::CameraPanning(int mX, int mY) {
 }
 
 void GameScreen_LevelEditor::RenderMapSprite(unsigned short sprite, int x, int y) {
-	switch (sprite) {
-	case TEST_SPRITE:
-		texture->Render(Vector2D(x, y), SDL_FLIP_NONE);
-		break;
-	case TEST_SPRITE_2:
-		texture2->Render(Vector2D(x, y), SDL_FLIP_NONE);
-		break;
-	}
+	if(sprite != SPRITE_CLEAR)
+		spritesheet[sprite]->Render(Vector2D(x, y), SDL_FLIP_NONE);
+}
+
+void GameScreen_LevelEditor::RenderCursorSprite(unsigned short sprite, int x, int y) {
+	spritesheet_cursor[sprite]->Render(Vector2D(x, y), SDL_FLIP_NONE);
 }
 
 void GameScreen_LevelEditor::ScreenToWorld(int screenPosX, int screenPosY, int &x, int &y) {
--- AnthonySturdy_Engine_GEC/AnthonySturdy_Engine_GEC/GameScreen_LevelEditor.h
+++ AnthonySturdy_Engine_GEC/AnthonySturdy_Engine_GEC/GameScreen_LevelEditor.h
@@ -8,6 +8,7 @@
 
 #include <vector>
 #include <iostream>
+#include <sstream>
 
 class GameScreen_LevelEditor : public GameScreen {
 public:
@@ -27,21 +28,28 @@ private:
 	int mapSizeX, mapSizeY;
 	std::vector<unsigned short> map;
 	void RenderMapSprite(unsigned short sprite, int x, int y);
+	void RenderCursorSprite(unsigned short sprite, int x, int y);
 	enum SPRITES {
-		CLEAR = 0,
-		TEST_SPRITE,
-		TEST_SPRITE_2
+		SPRITE_CLEAR = 0,
+		SPRITE_TEST,
+		SPRITE_TEST_2
 	};
+	unsigned short currentSprite = SPRITE_TEST;
 	void EditMap(unsigned short sprite, int x, int y);
+	void DrawCursor(unsigned short sprite, int x, int y);
 
 	//Camera Panning
 	void CameraPanning(int mX, int mY);
 	float cameraOffsetX = 0, cameraOffsetY = 0;
 	int startPanX = 0, startPanY = 0;
 
+	//Mouse button input
 	bool leftMouseDown = false;
 	bool middleMouseDown = false;
+	bool rightMouseDown = false;
 
 	Texture2D* texture;
 	Texture2D* texture2;
+	std::vector<Texture2D*> spritesheet;
+	std::vector<Texture2D*> spritesheet_cursor;
 };
